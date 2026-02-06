8edcbf2e4ac8eb145d315df15bd5e2e1451facd2
user <sinderellahunt@gmail.com>
2026-02-05 19:39:27 -0500

    Added API forPython HTTP Server.

---
+++ python_api/api_server.py
@@ -0,0 +1,49 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import pwd
+import grp
+from base64 import b64decode
+
+
+class APIHandler(BaseHTTPRequestHandler):
+    USERNAME = "test"
+    PASSWORD = "abcABC123456"
+
+    def do_POST(self):
+        # Check authentication
+        auth_header = self.headers.get("Authorization")
+        if not auth_header or not auth_header.startswith("Basic "):
+            self.send_response(401)
+            self.send_header("WWW-Authenticate", 'Basic realm="Login Required"')
+            self.end_headers()
+            return
+
+        encoded = auth_header.split()[1]
+        decoded = b64decode(encoded).decode()
+        username, password = decoded.split(":")
+        if username != self.USERNAME or password != self.PASSWORD:
+            self.send_response(403)
+            self.end_headers()
+            return
+
+
+        if self.path == "/api/users":
+            data = {str(i): u.pw_name for i, u in enumerate(pwd.getpwall())}
+        elif self.path == "/api/groups":
+            data = {str(i): g.gr_name for i, g in enumerate(grp.getgrall())}
+        else:
+            self.send_response(404)
+            self.end_headers()
+            return
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write(json.dumps(data).encode())
+
+
+if __name__ == "__main__":
+    server = HTTPServer(("", 3000), APIHandler)
+    print("Server running on port 3000...")
+    server.serve_forever()
+
